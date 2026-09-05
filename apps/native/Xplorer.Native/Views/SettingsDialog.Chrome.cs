16561c638835aa2d6f05eb53d04c63fe4df0cee8
Kate <dhwanksh2000@gmail.com>
2026-09-06 01:57:51 +0530

    fix(settings): wire titlebar close action

---
+++ apps/native/Xplorer.Native/Views/SettingsDialog.Chrome.cs
@@ -0,0 +1,8 @@
+using Microsoft.UI.Xaml;
+
+namespace Xplorer.Native.Views;
+
+public sealed partial class SettingsDialog
+{
+    private void SettingsClose_Click(object sender, RoutedEventArgs e) => Hide();
+}
